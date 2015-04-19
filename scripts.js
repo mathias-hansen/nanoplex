c302a15bf776d7d91ef48c4eb92d4036fa0552d1
Mathias Hansen <mathias.hansen783@gmail.com>
2015-04-20 01:47:30 +0200

    optimizing

--- scripts.js
+++ scripts.js
@@ -1,44 +1,54 @@
-(function(){
-	"use strict";
+(function () {
+    "use strict";
 
-	function changeView() {
-		var ajax = document.createElement("core-ajax"),
-			state = window.location.hash.replace(/^#\//, ""),
-			views = document.querySelector("#views"),
-			loading = views.querySelector("[view=loading]"),
-			missing = views.querySelector("[view=not-found]"),
-			page = views.querySelector("[view=page]"),
-			title = document.querySelector("title");
+    function changeView(hash) {
+        var view = hash.replace(/^#\//, "");
 
-		title.innerHTML = "nanoplex - " + state.replace(/-/g," ");
+        document.querySelector("title").innerHTML = "nanoplex - " + view.replace(/-/g," ");
 
-		page.setAttribute("hidden", "");
-		missing.setAttribute("hidden", "");
+        getView(view);
+    };
 
-		loading.removeAttribute("hidden");
+    function getView(view) {
+        var ajax = document.createElement("core-ajax"),
+            views = document.querySelector("#views").querySelectorAll("article"),
+            loading = views[0],
+            page = views[1];
 
-		ajax.url = "/views/" + state + ".html";
-		ajax.handleAs = "text";
-		ajax.generateRequest();
+        page.setAttribute("hidden", "");
+        loading.removeAttribute("hidden");
 
-		ajax.addEventListener("response", function(event) {
-			loading.setAttribute("hidden", "");
+        ajax.url = "/views/" + view + ".html";
+        ajax.handleAs = "text";
+        ajax.generateRequest();
 
-			if (event.detail == "Not found\n")
-				missing.removeAttribute("hidden");
-			else {
-				page.removeAttribute("hidden");
-				page.innerHTML = event.detail;
-			}
-		});
-	};
+        ajax.addEventListener("response", function(event) {
+            var response = event.detail;
 
-	window.addEventListener('hashchange', function(event) { 
-		changeView(); 
-	});
-	
-	if (window.location.hash === "")
-		window.location.href = "#/home";
-	else
-		changeView();
+            loading.setAttribute("hidden", "");
+            page.removeAttribute("hidden");
+            
+            if (response === "Not found\n") 
+                getView("missing");
+            else 
+                page.innerHTML = response;
+        });
+    };
+
+    window.onhashchange = function(event) { 
+        changeView(window.location.hash); 
+    };
+    
+    if (!window.location.hash) 
+        window.location.href = "#/home";
+    else 
+        changeView(window.location.hash);
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-47730446-1', 'auto');
+    ga('send', 'pageview');
+
 })();
